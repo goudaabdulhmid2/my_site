42566c5cd0195302d46163cf238f1e7a6a516517
Abdulhamid Gouda <goudaabdulhmid@gmail.com>
2026-04-18 19:46:47 +0200

    feat: Implement blog structure with dynamic posts and theme toggle
    - Added new templates for all posts, post details, and updated index page.
    - Created a JSON file to store blog posts data.
    - Implemented views to fetch and render posts dynamically.
    - Enhanced styling with a modern CSS framework and responsive design.
    - Introduced a theme toggle feature for light/dark mode.
    - Updated base template to include dynamic background effects and navbar.
    - Added a new navbar component for navigation.
    - Updated .gitignore to exclude images and prettierignore.

--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -1,14 +1,37 @@
 from django.shortcuts import render
+import json
+from pathlib import Path
 
-# Create your views here.
+# Create your views here
+# read data from json
+def get_all_posts():
+    posts_file = Path(__file__).resolve().parent.parent / "dummy_posts.json"
+    try:
+        with posts_file.open("r", encoding="utf-8") as file:
+            data = file.read()
+            return json.loads(data)
+    except (FileNotFoundError, json.JSONDecodeError):
+        return []
 
+def get_date(post):
+    return post["date"]
 
 def starting_page(request):
-    return render(request,'blog/index.html')
+    all_posts = get_all_posts()
+    sorted_posts = sorted(all_posts, key=get_date, reverse=True)
+    least_three_posts = sorted_posts[:3]
+
+    return render(request, 'blog/index.html', context={
+        "posts": least_three_posts
+    })
 
 
 def posts(request):
-    return render()
+    all_posts = get_all_posts()
+    return render(request, 'blog/all-posts.html', context={
+        "posts": all_posts
+    })
 
 def post_detail(request, slug):
-    return render()
+
+    return render(request,"blog/post-details.html")
